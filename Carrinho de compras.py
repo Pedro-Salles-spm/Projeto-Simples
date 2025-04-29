990f7bf318e8d54407992dcfe62daec55360d8ee
Pedro._.Sales <salespedro004@gmail.com>
2025-04-29 17:06:18 -0300

    Add files via upload

---
+++ Carrinho de compras.py
@@ -0,0 +1,12 @@
+nome = input("digite seu nome:")
+
+produto = input('digite o nome do produto:')
+
+preço = float(input('digite o preço do produto:'))
+
+quntidade = int(input('digite a quantidade do produto:'))
+
+total = preço * quntidade 
+
+print(f'ola {nome}, o produto {produto} esta no valor de {preço} e a quantidade de {quntidade} ficaria no valor de  {total}, Obrigado pela compra')
+
