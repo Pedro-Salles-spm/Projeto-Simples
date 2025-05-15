b13becb9c8215984f8edd3e69a68001367227b97
Pedro._.Sales <salespedro004@gmail.com>
2025-05-14 21:31:28 -0300

    Add files via upload

--- media_em_loop.py
+++ media_em_loop.py
@@ -2,18 +2,24 @@ alunos = []
 
 for i in range(5):
     nome = input('digite seu nome:')
-    av1 = float(input('digite a nota da av1:'))
-    av2 = float(input('digite a nota da av2:'))
+    av1 = float(input('\ndigite a nota da av1:'))
+    av2 = float(input('\ndigite a nota da av2: '))
+    print('\n')
     
     media = (av1 + av2) /2
     
-    alunos.append({
-        'nome': nome,
-        'av1':av1,
-        'av2': av2,
-        'media': media
-    })
-    print('\n ----resultado final ----')
+    if media >= 6:
+        print(f'parabens {nome},vecê foi aprovado com a media {media:.2f}')
+        
+    else:
+        print(f'Que pena {nome}, você foi reprovamdo com a media {media:.2f}')
+        
+    alunos.append([nome,av1,av2,media])
+    print('\n ----resultado final---- \n')
     
     for aluno in alunos:
-        print(f'nome: {aluno['nome']}, - avaliação1: {aluno['av1']}, - avaliação2: {aluno['av2']}, - media: {aluno['media']:.2f}')
+        
+      if aluno[3] >= 6:
+        print(f'nome: {aluno[0]}, - avaliação1: {aluno[1]}, - avaliação2: {aluno[2]}, - media: {aluno[3]:.2f} - aprovado')
+      else:
+        print(f'nome: {aluno[0]}, - avaliação1: {aluno[1]}, - avaliação2: {aluno[2]}, - media: {aluno[3]:.2f} - reprovado')
